1e48e351ce153399edc6134525ebd75d582acab6
Zoran Bajic <zoranbajic@protonmail.com>
2021-11-30 20:21:17 -0600

    chore: add contact section and install babel/plugin-transform-runtime to get async axios post method to work

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -1,3 +1,117 @@
-function Contact() {}
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Contact() {
+  const [messageInfo, setMessageInfo] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  function handleChange(evt) {
+    setMessageInfo({ ...messageInfo, [evt.target.name]: evt.target.value });
+  }
+
+  async function Submit(evt) {
+    // Prevent the default behavior
+    evt.preventDefault();
+    // Post the message to 99inbound with their required headers per their docs
+    try {
+      const headers = {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      };
+      const res = await axios.post(
+        'https://app.99inbound.com/api/e/GFrmlRV9',
+        messageInfo,
+        { headers }
+      );
+      setMessageInfo({
+        name: '',
+        email: '',
+        message: '',
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <section id='contact' className='relative'>
+      <div className='container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap'>
+        <form
+          name='contact'
+          className='lg:w-1/3 md:w-1/2 flex flex-col md:mx-auto w-full md:py-8 mt-8 md:mt-0'
+          target='_blank'
+          onSubmit={Submit}
+        >
+          <h2 className='text-white sm:text-4xl text-3xl mb-4 font-medium title-font self-center'>
+            Contact Me
+          </h2>
+          <p className='leading-relaxed mb-5 text-center'>
+            I'm open to work so feel free to reach out if you have an
+            opportunity that you think would be a good fit!
+          </p>
+          <div className='relative mb-4'>
+            <label htmlFor='name' className='leading-7 text-sm text-gray-400'>
+              Name
+            </label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              value={messageInfo.name}
+              onChange={handleChange}
+              className='w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+          <div className='relative mb-4'>
+            <label htmlFor='email' className='leading-7 text-sm text-gray-400'>
+              Email
+            </label>
+            <input
+              type='text'
+              id='email'
+              name='email'
+              value={messageInfo.email}
+              onChange={handleChange}
+              className='w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-8 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+          <div className='relative mb-4'>
+            <label
+              htmlFor='message'
+              className='leading-7 text-sm text-gray-400'
+            >
+              Message
+            </label>
+            <textarea
+              id='message'
+              name='message'
+              value={messageInfo.message}
+              onChange={handleChange}
+              className='w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out'
+            />
+          </div>
+          <button
+            type='submit'
+            className='text-white bg-indigo-500 border-0 py-2 px-6 foucs:outline-none hover:bg-indigo-600 rounded text-lg'
+          >
+            Send
+          </button>
+          <div style={{ position: 'absolute', left: '-5000px' }}>
+            <input
+              type='checkbox'
+              name='splendid_chiffon_fractal_shirt'
+              value='1'
+              tabIndex='-1'
+              autoComplete='no'
+            />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
 
 export default Contact;
--- client/components/Footer.js
+++ client/components/Footer.js
@@ -36,13 +36,8 @@ function Footer() {
                 <a href='#skills'>Skills</a>
               </h2>
             </div>
-            {/* <div className='lg:w-1/4 md:w-1/2 w-full px-4'>
-              <h2 className='title-font font-bold text-white tracking-widest text-md mb-3'>
-                Contact
-              </h2>
-            </div> */}
             <div className='lg:w-1/3 md:w-1/2 w-full'>
-              <span className='inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-center'>
+              <span className='inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-center sm:mb-10'>
                 <a
                   className='ml-3 text-gray-200'
                   href='https://twitter.com/ADevToBe'
@@ -85,47 +80,10 @@ function Footer() {
 
         <div className='bg-gray-800'>
           <div className='container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row justify-center'>
-            <p className='text-white text-sm text-center sm:text-left'>
+            <p className='text-white text-sm text-center sm:text-left md:mt-auto mt-10'>
               Copyright © Zoran Bajic {new Date().getFullYear()}
               {'.'}
             </p>
-            {/* <span className='inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start'>
-              <a
-                className='ml-3 text-gray-200'
-                href='https://twitter.com/ADevToBe'
-              >
-                <svg
-                  fill='currentColor'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  className='w-5 h-5'
-                  viewBox='0 0 24 24'
-                >
-                  <path d='M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z'></path>
-                </svg>
-              </a>
-              <a
-                className='ml-3 text-gray-200'
-                href='https://www.linkedin.com/in/zoranbajic'
-              >
-                <svg
-                  fill='currentColor'
-                  stroke='currentColor'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='0'
-                  className='w-5 h-5'
-                  viewBox='0 0 24 24'
-                >
-                  <path
-                    stroke='none'
-                    d='M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z'
-                  ></path>
-                  <circle cx='4' cy='4' r='2' stroke='none'></circle>
-                </svg>
-              </a>
-            </span> */}
           </div>
         </div>
       </footer>
