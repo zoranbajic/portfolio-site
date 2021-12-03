4db58852b17fae39f831802e4fa15fe426e1a989
Zoran Bajic <zoranbajic@protonmail.com>
2021-12-02 19:13:16 -0600

    chore: change button text in About component

--- client/components/About.js
+++ client/components/About.js
@@ -22,7 +22,7 @@ function App() {
               href='#contact'
               className='inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg'
             >
-              Work With Me
+              Get In Touch
             </a>
             <a
               href='#projects'
