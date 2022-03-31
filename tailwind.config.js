c26752d9a370f3811688607327054b64dd0d665f
Zoran Bajic <zoranbajic@protonmail.com>
2022-03-30 20:30:37 -0500

    chore: update tailwindcss from 2.2.19 to 3.0.23

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [],
   theme: {
     extend: {},
   },
@@ -8,4 +7,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
